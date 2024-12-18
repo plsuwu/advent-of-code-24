f16352b16dda5115c144cb30cd825e76443e1fba
plsuwu <124419933+plsuwu@users.noreply.github.com>
2024-12-19 00:17:46 +1000

    day five part two

--- five/src/main.rs
+++ five/src/main.rs
@@ -14,10 +14,52 @@ impl Updates {
     }
 
     fn add_rule(&mut self, a: u32, b: u32) {
-        self.rules
-            .entry(a)
-            .or_insert_with(|| HashSet::new())
-            .insert(b);
+        self.rules.entry(a).or_insert_with(HashSet::new).insert(b);
+    }
+
+    fn correct_ordering(&self, update: &[u32]) -> Vec<u32> {
+        let mut graph: HashMap<u32, HashSet<u32>> = HashMap::new();
+        let pages: HashSet<_> = update.iter().copied().collect();
+
+        for &page in &pages {
+            if let Some(after_pages) = self.rules.get(&page) {
+                for &after in after_pages {
+                    if pages.contains(&after) {
+                        graph.entry(page).or_insert_with(HashSet::new).insert(after);
+                    }
+                }
+            }
+        }
+
+        let mut in_deg: HashMap<u32, usize> = pages.iter().map(|&page| (page, 0)).collect();
+        for (_, after_pages) in &graph {
+            for &after in after_pages {
+                *in_deg.entry(after).or_insert(0) += 1;
+            }
+        }
+
+        let mut res = Vec::new();
+        let mut queue: VecDeque<_> = in_deg
+            .iter()
+            .filter(|(_, &count)| count == 0)
+            .map(|(&page, _)| page)
+            .collect();
+
+        while let Some(page) = queue.pop_front() {
+            res.push(page);
+
+            if let Some(after_pages) = graph.get(&page) {
+                for &after in after_pages {
+                    let count = in_deg.get_mut(&after).unwrap();
+                    *count -= 1;
+                    if *count == 0 {
+                        queue.push_back(after);
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 
     fn valid_ordering(&self, update: &Vec<u32>) -> bool {
@@ -57,11 +99,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         .for_each(|u| {
             let update: Vec<u32> = u.split(',').map(|u| u.parse::<u32>().unwrap()).collect();
 
-            if man.valid_ordering(&update) {
-                acc += update[update.len() / 2];
+            if !man.valid_ordering(&update) {
+                let corr = man.correct_ordering(&update);
+                let mid = corr.len() / 2;
+
+                acc += corr[mid];
             }
         });
 
     println!("{:?}", acc);
+
     return Ok(());
 }
